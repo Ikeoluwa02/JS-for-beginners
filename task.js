0f536dc249f9e994d5ef40b1f663bdb91c73efee
Adedoyin Ikeoluwa <105397305+Ikeoluwa02@users.noreply.github.com>
2022-06-25 09:26:20 -0700

    Add files via upload
    This is a task for Javascript Beginners.

---
+++ task.js
@@ -0,0 +1,12 @@
+const name = 'Ikeoluwa';
+const object = '1.43m';
+const country = 'Nigeria';
+
+console.log(`My name is ${name}. I am about ${object} tall and I am from ${country}.`)
+
+
+alert(` ${name}`),
+alert(`${object}`),
+alert(`${country}`);
+
+
